62e664e018a2a992f0644edbc81186337b514540
Rui C. Monteiro <ruicunham@hotmail.com>
2025-06-17 12:04:45 +0100

    feat: bold score text (#2)
    * feat: make score text bold
    
    * chore(readme): update images

--- app/src/main/java/volleyballscore/components/ScoreCounter.kt
+++ app/src/main/java/volleyballscore/components/ScoreCounter.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.Text
@@ -22,17 +23,20 @@ fun ScoreCounter(modifier: Modifier, scoreCount: SnapshotStateList<List<Int>>) {
                 .weight(1f),
             textAlign = TextAlign.Right,
             fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
             text = "${scoreCount.last()[0]}",
         )
         Text(
             textAlign = TextAlign.Center,
             fontSize = 50.sp, text = " - ",
+            fontWeight = FontWeight.Bold,
         )
         Text(
             modifier = Modifier
                 .weight(1f),
             textAlign = TextAlign.Left,
             fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
             text = "${scoreCount.last()[1]}",
         )
     }
